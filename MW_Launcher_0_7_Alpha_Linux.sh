f8e6dcb868679f411b23f9ab0b3e66aa545e7a2f
linuxfanatic1 <vgfh.hj@gmail.com>
2016-08-01 13:10:39 +0800

    Minor wording changes

--- MW_Launcher_0_7_Alpha_Linux.sh
+++ MW_Launcher_0_7_Alpha_Linux.sh
@@ -2,9 +2,9 @@
 #This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
-#NFS MW 1.4 and 2.0  Community Patch Launcher for Linux
+#Need for Speed Most Wanted 1.4 and 2.0 Community Patch Launcher for UNIX-Like Operating Systems
 #Version 0.7.1 BETA Codename 'Silvia'
-#DO NOT DISTRIBUTE THIS BETA VERSION!!!!!111
+#DO NOT DISTRIBUTE THIS BETA VERSION UNDER ANY CIRCUMSTANCE WITHOUT THE EXPLICIT PERMISSION OF EITHER LINUXFANATIC/HARU OR ARTUROPLAYERONE/ARTURO!
 #Copyleft Haru Jayasekara and MW 1.4 Patch Team 2016
 #Do whatever you want with this, I don't care, as long as you redistribute your modified files under a FSF-certified License.
 echo -e NFSMW 1.4 and 2.0 Community Patch Launcher for UNIX-Like Operating Systems
@@ -15,7 +15,7 @@ echo -e Linux Build 19/7/16
 sleep 1
 echo -e Version 0.7.1 BETA Codename 'Silvia'
 sleep 1
-echo -e DO NOT distribute this BETA version.
+echo -e DO NOT distribute this BETA version, see comments in 
 sleep 1
 echo -e Check out the patch thread at http://www.gtrcars.net/forum/showthread.php?t=1948
 sleep 1
