f673c419c6232cb1fe6baa3f74f8e666f2f06c5a
linuxfanatic1 <vgfh.hj@gmail.com>
2016-09-10 22:24:22 +0800

    Update MW_Launcher_Redist.sh

--- 0_7_Silvia/MW_Launcher_Redist.sh
+++ 0_7_Silvia/MW_Launcher_Redist.sh
@@ -1,22 +1,22 @@
-
 #!/bin/bash
 #This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
-#Need for Speed Most Wanted for UNIX-Like Operating Systems
-#Version 0.7.1 Release Candidate 1 Codename 'Silvia'
-#Copyleft Haru Jayasekara 2016
+#Need for Speed Most Wanted Launcher for UNIX-Like Operating Systems
+#Version 0.7.2 Release
+#Copyleft Haru Jayasekara
 #I recommend using the R.G. Mechanics version of NFSMW. 
 #This launcher is preconfigured for it but you can edit the lines for options 3 and 4
 #to use a different version.
 MWDIR="/home/$USER/PlayOnLinux's virtual drives/NFSMW1.4/drive_c/Program Files/R.G. Mechanics/Need for Speed - Most Wanted/scripts/"
+EOFILE=NFSMWExtraOptions.asi
 echo -e NFS MW Launcher for UNIX-Like Operating Systems
 sleep 1
-echo -e Copyleft Haru Jayasekara 2016 under MPL 2.0 License
+echo -e Copyleft Haru Jayasekara and MW 1.4 Patch Team 2016 under MPL 2.0 License
 sleep 1
-echo -e Linux Build 17/8/16
+echo -e Linux Build 9/10/16
 sleep 1
-echo -e Version 0.7.1 Release Candidate 1 Codename 'Silvia'
+echo -e Version 0.7.2
 sleep 1
 echo -e Check out the patch thread at http://www.gtrcars.net/forum/showthread.php?t=1948
 sleep 1
@@ -35,17 +35,12 @@ case "$choice" in
 #Edit the next line to run without Mod Loader but with ExOpts
   2 ) /usr/share/playonlinux/playonlinux --run "speed" %F;;
 #Edit the next line to run with Mod Loader but without ExOpts
-  3 ) cd "$MWDIR" && mv NFSMWExtraOptions.asi /home/$USER/Documents && /usr/share/playonlinux/playonlinux --run "speed" %F -mod && cd /home/$USER/Documents && mv NFSMWExtraOptions.asi "$MWDIR";;
+  3 ) cd "$MWDIR" && mv "$EOFILE" /home/$USER/Documents && /usr/share/playonlinux/playonlinux --run "speed" %F -mod && cd /home/$USER/Documents && mv "$EOFILE" "$MWDIR";;
 #Edit the next line to run without Mod Loader and without ExOpts
-  4 ) cd "$MWDIR" && mv NFSMWExtraOptions.asi /home/$USER/Documents && /usr/share/playonlinux/playonlinux --run "speed" %F && cd /home/$USER/Documents && mv NFSMWExtraOptions.asi "$MWDIR";;
+  4 ) cd "$MWDIR" && mv "$EOFILE" /home/$USER/Documents && /usr/share/playonlinux/playonlinux --run "speed" %F && cd /home/$USER/Documents && mv "$EOFILE" "$MWDIR";;
   * ) echo "Invalid, please launch again";;
 esac
 sleep 1
 exit
 
 
-
-
-esac
-sleep 1
-exit
